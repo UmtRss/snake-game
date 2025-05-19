6f8630fc3dd47f337e60f7daf93cec2a389800e8
UmtRss <umtrss@gmail.com>
2025-05-19 18:32:01 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -7,7 +8,6 @@ LEFT = 180
 RIGHT = 0
 
 class Snake:
-
     def __init__(self):
         self.segments = []
         self.create_snake()
@@ -17,17 +17,22 @@ class Snake:
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
-
     def add_segment(self, position):
-        new_segment = Turtle("square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.goto(position)
-        self.segments.append(new_segment)
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000, 1000)  # ekran dışına gönder
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
