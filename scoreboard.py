6ec2500c56e130e7bf24cb8941433b4a8a705f5f
UmtRss <umtrss@gmail.com>
2025-05-19 18:31:39 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,28 +1,42 @@
 from turtle import Turtle
-ALIGNMENT = "center"
-FONT = ("Arial", 24, "normal")
 
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
-
     def __init__(self):
         super().__init__()
-        self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 270)
         self.hideturtle()
+        self.goto(0, 260)
+        self.score = 0
+
+        # highscore.txt dosyasını oku
+        try:
+            with open("highscore.txt") as file:
+                self.high_score = int(file.read())
+        except FileNotFoundError:
+            self.high_score = 0
+
         self.update_scoreboard()
 
     def update_scoreboard(self):
-         self.write(f"Score {self.score}", align="center", font=("Arial", 20, "normal"))
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("highscore.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
